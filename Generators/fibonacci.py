22c67f169d4ea4433bbc7aae5a18d09c3fa24122
Paremuzyan <128408546+Paremuzyan@users.noreply.github.com>
2023-03-31 17:48:05 +0400

    Updated fibonacci.py

--- Generators/fibonacci.py
+++ Generators/fibonacci.py
@@ -1,16 +1,12 @@
 def fib_sequance(num):
     num_1, num_2 = 0, 1
-    for i in range(num):
+    i = 0
+    while i < num:
         num_1, num_2 = num_2, num_1 + num_2
+    
         yield num_1
+        i += 1
 
 
-# for fib_number in fib_sequance(10):
-#     print(fib_number, end=', ')
-# print()
-
-fib = fib_sequance(10)
-default_val = 'done!'
-for i in range(12):
-    print(next(fib, default_val))
-
+for i in fib_sequance(12):
+    print(i)
