5ce1a94de3c1eda71981ae07b9cf1b7476d2174c
Paremuzyan <aparemuzyan88@gmail.com>
2023-04-09 23:08:36 +0400

    Fixed bug in logging.py .

--- decorators/logging_dec.py
+++ decorators/logging_dec.py
@@ -1,16 +1,17 @@
 def logging_dec(f):
-    def info(*args):
+    def info(*args, **kwargs):
         print(f"The function name is {f.__name__}")
         print(f"The positional arguments are {args}")
-        return_val = f(*args)
+        print(f"The key arguments are {kwargs}")
+        return_val = f(*args) if args else f(**kwargs)
         print(f"The result is {return_val}")
     return info
 
 
 @logging_dec
-def add(val_1, val_2):
+def add(val_1, val_2=6):
     return val_1 + val_2
 
 
 if __name__ == "__main__":
-    add(1, 5)
+    add(val_2=7, val_1=1)
