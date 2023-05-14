22c435d8b05ecfa056ac17f0fa8005e6e7f2e488
Paremuzyan <aparemuzyan88@gmail.com>
2023-05-14 23:58:42 +0400

    Updated set class.

--- classes/set.py
+++ classes/set.py
@@ -1,3 +1,24 @@
 class MySet:
-    def __init__(self):
+    def __init__(self, *args):
+        self.set_value(args)
+
+    def set_value(self, args):
+        my_set = []
+        for arg in args:
+            if arg not in my_set:
+                my_set.append(arg)
+        self.__set = set(my_set)
+
+    def __str__(self):
+        return str(self.__set)
+
+    def add(self):
         pass
+
+    def clear(self):
+        pass
+
+
+
+s = MySet(1,2,3,4,5,5)
+print(s)
